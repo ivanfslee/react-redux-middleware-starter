caddbbd4cddc49de87b09db54999df8d18ea1483
ivanfslee <ivanfslee@gmail.com>
2019-12-21 15:57:19 -0800

    Added redux-promise to handle async http get requests

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
+import reduxPromise from 'redux-promise';
 
-const theStore = createStore(rootReducer);
+// applyMiddleware(reduxPromise) returns a function 
+// we pass createStore into it - which also returns a function
+// we pass rootReducer into that function 
+const theStore = applyMiddleware(reduxPromise)(createStore)(rootReducer);
 
 ReactDOM.render(
     <Provider store={theStore}>
